7171a62f29520440583174001c203fb1dd8a30e6
fuqunaga <fukhid@gmail.com>
2019-07-08 18:48:08 +0900

    ResizableWindow supports cursor

--- Assets/RapidGUI/RGUI/ResizableWindow.cs
+++ Assets/RapidGUI/RGUI/ResizableWindow.cs
@@ -73,41 +73,93 @@ namespace RapidGUI
                 return GUILayout.Window(id, rect, func, text, CheckStyle(style), options);
             }
 
+
             Rect ResizeRect(Rect window, float detectionRange)
             {
-                Event current = Event.current;
-
-                if (current.type == EventType.MouseUp)
+                Rect CalcDraggableRect(Rect r, float d)
                 {
-                    draggingLR = draggingTB = 0;
+                    r.xMin -= d;
+                    r.yMin -= d;
+                    r.xMax += d;
+                    r.yMax += d;
+                    return r;
                 }
-                else if (current.type == EventType.MouseDown)
+
+                var evt = Event.current;
+                var id = GUIUtility.GetControlID(FocusType.Passive);
+
+                switch (evt.type)
                 {
-                    Rect resizer = window;
-                    var pos = current.mousePosition;
-
-                    if (draggingLR == 0 && draggingTB == 0)
-                    {
-                        resizer.xMin -= detectionRange;
-                        resizer.yMin -= detectionRange;
-                        resizer.xMax += detectionRange;
-                        resizer.yMax += detectionRange;
-
-                        if (resizer.Contains(current.mousePosition))
+                    case EventType.MouseUp:
                         {
-                            draggingLR = (pos.x < window.xMin) ? -1 : ((window.xMax < pos.x) ? 1 : 0);
-                            draggingTB = (pos.y < window.yMin) ? -1 : ((window.yMax < pos.y) ? 1 : 0);
+                            draggingLR = draggingTB = 0;
+                            if (GUIUtility.hotControl == id) GUIUtility.hotControl = 0;
                         }
-                    }
-                }
-                else if (current.type == EventType.MouseDrag && current.button == 0)
-                {
-                    var pos = current.mousePosition;
+                        break;
 
-                    if (draggingLR == -1) window.xMin = pos.x;
-                    if (draggingLR == 1) window.xMax = pos.x;
-                    if (draggingTB == -1) window.yMin = pos.y;
-                    if (draggingTB == 1) window.yMax = pos.y;
+                    case EventType.MouseDown:
+                        {
+                            if (GUIUtility.hotControl == 0)
+                            {
+                                var pos = evt.mousePosition;
+
+                                var rect = CalcDraggableRect(window, detectionRange);
+
+                                if (rect.Contains(pos))
+                                {
+                                    draggingLR = (pos.x < window.xMin) ? -1 : ((window.xMax < pos.x) ? 1 : 0);
+                                    draggingTB = (pos.y < window.yMin) ? -1 : ((window.yMax < pos.y) ? 1 : 0);
+
+                                    GUIUtility.hotControl = id;
+                                }
+                            }
+                        }
+                        break;
+
+                    case EventType.MouseDrag:
+                        if ((GUIUtility.hotControl == id) && (evt.button == 0))
+                        {
+                            var pos = evt.mousePosition;
+
+                            if (draggingLR == -1) window.xMin = pos.x;
+                            if (draggingLR == 1) window.xMax = pos.x;
+                            if (draggingTB == -1) window.yMin = pos.y;
+                            if (draggingTB == 1) window.yMax = pos.y;
+                        }
+                        break;
+
+                    case EventType.Repaint:
+                        {
+                            var cursor = MouseCursor.Default;
+
+                            if (GUIUtility.hotControl == 0)
+                            {
+                                var pos = evt.mousePosition;
+                                var rect = CalcDraggableRect(window, detectionRange);
+                                if (rect.Contains(pos) && !window.Contains(pos))
+                                {
+                                    var h = (pos.x < window.xMin) || (window.xMax < pos.x);
+                                    var v = (pos.y < window.yMin) || (window.yMax < pos.y);
+
+                                    cursor = h
+                                         ? (v ? MouseCursor.ResizeUpLeft : MouseCursor.ResizeHorizontal)
+                                         : (v ? MouseCursor.ResizeVertical : MouseCursor.Default);
+
+                                }
+                            }
+                            else
+                            {
+                                cursor = (draggingLR != 0)
+                                    ? ((draggingTB != 0) ? MouseCursor.ResizeUpLeft : MouseCursor.ResizeHorizontal)
+                                    : ((draggingTB != 0) ? MouseCursor.ResizeVertical : MouseCursor.Default);
+                            }
+
+                            if (cursor != MouseCursor.Default)
+                            {
+                                RGUIUtility.SetCursor(cursor);
+                            }
+                        }
+                        break;
                 }
 
                 return window;
