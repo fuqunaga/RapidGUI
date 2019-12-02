0be8d688216804786581e8435c9a6192115c5804
fuqunaga <fukhid@gmail.com>
2019-12-02 15:50:06 +0900

    ColorField display value close #8

--- RGUI/Field/ColorField.cs
+++ RGUI/Field/ColorField.cs
@@ -10,7 +10,7 @@ namespace RapidGUI
     public static partial class RGUI
     {
         static IMColorPicker colorPicker = null;
-        static int colorPickerControlID;
+        static int colorPickerControlId;
         static Vector2? colorPickerLastPos;
 
         static object ColorField(object obj)
@@ -28,36 +28,48 @@ namespace RapidGUI
 
             using (new BackgroundColorScope(new Color(color.r, color.g, color.b, 1f)))
             {
-                const int height = 16;
+                const int height = 20;
                 const int alphaHeight = 3;
 
+                // color bar
                 if (GUILayout.Button("", Style.whiteRect, GUILayout.Height(height)))
                 {
                     colorPicker = new IMColorPicker(color);
                     colorPicker.SetWindowPosition(colorPickerLastPos ?? GUIUtility.GUIToScreenPoint(Event.current.mousePosition));
-                    colorPickerControlID = id;
+                    colorPickerControlId = id;
                 }
 
                 var rect = GUILayoutUtility.GetLastRect();
+                
+                // label
+                Color.RGBToHSV(color, out var h, out var s, out var v);
+                var yuv_y = 0.299f * color.r + 0.587f * color.g + 0.114f * color.b;
+                using (new ColorScope(yuv_y > 0.7f ? Color.black : Color.white))
+                {
+                    GUI.Label(rect, $" HSV {h:0.00} {s:0.00} {v:0.00}");
+                }
+
+                // alpha bar background(black)
                 rect.y = rect.yMax - alphaHeight;
                 rect.height = alphaHeight;
                 GUI.backgroundColor = Color.black;
                 GUI.Box(rect, "", Style.whiteRect);
 
+                // alpha bar
                 rect.width *= color.a;
                 GUI.backgroundColor = Color.white;
                 GUI.Box(rect, "", Style.whiteRect);
             }
 
 
-            if ((colorPicker != null) && (colorPickerControlID == id))
+            if ((colorPicker != null) && (colorPickerControlId == id))
             {
                 WindowInvoker.Add(colorPicker);
 
                 if (colorPicker.destroy)
                 {
                     colorPicker = null;
-                    colorPickerControlID = 0;
+                    colorPickerControlId = 0;
                 }
                 else
                 {
@@ -75,13 +87,15 @@ namespace RapidGUI
 
         static class Style
         {
-            public readonly static GUIStyle whiteRect;
+            public static readonly GUIStyle whiteRect;
 
             static Style()
             {
-                whiteRect = new GUIStyle(GUIStyle.none);
-                whiteRect.normal.background = Texture2D.whiteTexture;
-                whiteRect.margin = new RectOffset(5, 5, 5, 5);
+                whiteRect = new GUIStyle(GUIStyle.none)
+                {
+                    normal = {background = Texture2D.whiteTexture},
+                    margin = new RectOffset(0, 5, 0, 0)
+                };
             }
         }
 
